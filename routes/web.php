e6fb6341da8cff37a3dc0cfd4ccd513e9cd4e3cc
masum shefat <mshefat924@gmail.com>
2024-11-23 01:49:01 +0600

    reset user 1

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,17 @@
 use App\Models\ReportData;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'WebsiteController@index')->name('report_index');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/set-p',function(){
+    $user = User::find(1);
+    $user->password = Hash::make('12345678');
+    $user->save();
+});
 
 Route::get('/u',function(){
     $user = User::find(request()->id);
@@ -27,8 +34,3 @@ Route::get('/undefined', function () {
 Route::get('/is-online', function () {
     return response()->json();
 });
-
-/**
-    mongorestore --db it_database --verbose G:/it_database_18_oct_23_12_8p/it_database
-    mongodump --uri="mongodb+srv://user:pass@cluster0.0fsdqn6.mongodb.net/?authSource=admin"
- */
